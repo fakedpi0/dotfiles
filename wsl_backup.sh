47488eefe038db91e7f183a776d81951b91a4080
dpi0 <dpi0.dev@proton.me>
2023-10-17 13:44:15 +0530

    reindexed all files

--- wsl_backup.sh
+++
@@ -1,6 +0,0 @@
-rsync -avz  ~/.zshrc /mnt/c/Users/divyansh/Documents/Backup\ Settings\ \&\ Files/wsl_backup/.zshrc
-rsync -avz  ~/wsl_backup.sh /mnt/c/Users/divyansh/Documents/Backup\ Settings\ \&\ Files/wsl_backup/wsl_backup.sh
-rsync -avz  ~/.tmux.conf.local /mnt/c/Users/divyansh/Documents/Backup\ Settings\ \&\ Files/wsl_backup/.tmux.conf.local
-rsync -rva --delete .config /mnt/c/Users/divyansh/Documents/Backup\ Settings\ \&\ Files/wsl_backup/
-
-# nice
